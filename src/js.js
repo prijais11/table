aa4ee72ce7de812d0ae49bc4ed7f53e4dbfec17d
prijais11 <priyajaiswal@cedcoss.com>
2022-02-05 11:50:16 +0530

    Task 11

--- src/js.js
+++ src/js.js
@@ -36,3 +36,4 @@ const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
+const sum = 10 + 10;
