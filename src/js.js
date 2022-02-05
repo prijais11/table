f9babc0c2151821530775811dd399b4e3c7b909c
prijais11 <priyajaiswal@cedcoss.com>
2022-02-05 13:00:31 +0530

    Task 27

--- src/js.js
+++ src/js.js
@@ -80,3 +80,4 @@ b /= 4;
 c /= 11;
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
