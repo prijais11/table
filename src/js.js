6dcfcf471b01377573bd6841056a3a866afa8563
prijais11 <priyajaiswal@cedcoss.com>
2022-02-05 14:27:09 +0530

    Task 36

--- src/js.js
+++ src/js.js
@@ -111,3 +111,7 @@ myStr = "Hello World"; // Change this line
 const lastName = "Lovelace";
 // Only change code below this line
 const thirdLetterOfLastName = lastName[2]; // Change this line
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
