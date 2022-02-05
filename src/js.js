3e8278fea4b85934a4e8ac3fbfe886c3a0cb6a92
prijais11 <priyajaiswal@cedcoss.com>
2022-02-05 11:31:57 +0530

    Task 7

--- src/js.js
+++ src/js.js
@@ -15,3 +15,12 @@ b=a;
 var a=9;
 var myFirstName="Priya";
 var myLastName="Jaiswal"
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
