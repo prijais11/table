267f5f8545faa4bcc8c73549c7b75c354ec0f63d
prijais11 <priyajaiswal@cedcoss.com>
2022-02-05 14:23:58 +0530

    Task 35

--- src/js.js
+++ src/js.js
@@ -107,3 +107,7 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
