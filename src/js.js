5edadc71535f65b40293f2cf77b39c350abf3bb2
prijais11 <priyajaiswal@cedcoss.com>
2022-02-05 14:05:57 +0530

    Task 32

--- src/js.js
+++ src/js.js
@@ -91,3 +91,8 @@ const myStr = "My name is "+myName+" and I am well!";
 const someAdjective = "cool!";
 let myStr = "Learning to code is ";
 myStr += someAdjective;
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
