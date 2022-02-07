ffeacf4367dc96abaa503da19b389a2af063ba42
prijais11 <priyajaiswal@cedcoss.com>
2022-02-07 09:30:32 +0530

    Task 37

--- src/js.js
+++ src/js.js
@@ -115,3 +115,7 @@ const thirdLetterOfLastName = lastName[2]; // Change this line
 const lastName = "Lovelace";
 // Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line
