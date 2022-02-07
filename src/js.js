23ec4d6963debdcd3fbaf83247327c80b776566e
prijais11 <priyajaiswal@cedcoss.com>
2022-02-07 09:37:59 +0530

    Task 38

--- src/js.js
+++ src/js.js
@@ -119,3 +119,10 @@ const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
 const lastName = "Lovelace";
 // Only change code below this line
 const secondToLastLetterOfLastName = lastName[lastName.length -2]; // Change this line
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+// Only change code below this line
+const wordBlanks = myNoun+" "+myAdjective+" "+myVerb+" "+myAdverb; // Change this line
+// Only change code above this line
