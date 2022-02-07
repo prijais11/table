c8ad9baad3d36702ef6bd2839ce3f09b81718aa1
prijais11 <priyajaiswal@cedcoss.com>
2022-02-07 09:57:07 +0530

    Task 42

--- src/js.js
+++ src/js.js
@@ -132,3 +132,7 @@ const myArray = ["hey",3];
 const myArray = [["hey",3],["you",9]];
 const myArray = [50, 60, 70];
 const myData =myArray[0];
+// Setup
+const myArray = [15, 64, 99];
+myArray[0]=45;
+// Only change code below this line
